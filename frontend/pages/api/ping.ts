168f25ae4b20837546bfd5bf64c21c49a26373fa
Yudistira Maulana <yudistir4maulana@gmail.com>
2023-05-03 12:54:20 +0700

    ping api

---
+++ frontend/pages/api/ping.ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { connectToDatabase } from '@/database/connection';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+interface Response {
+  data?: any;
+  message?: string;
+  error?: string;
+}
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Response>
+) {
+  await connectToDatabase();
+  switch (req.method) {
+    case 'GET':
+      res.status(200).json({ data: 'Server Ok' });
+  }
+}
